1a9c419b2605fbcb81714cb59d98277937474482
王浩 <1536001366@qq.com>
2018-06-20 18:08:50 +0800

    在webpack中加了个无用代码清楚插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');  // 加了个无用代码清楚插件
+
 module.exports = {
   entry: './src/app.js',
   devtool: 'sourcemap',
@@ -12,9 +14,12 @@ module.exports = {
       test: /\.js$/,
       include: path.join(__dirname, 'src'),
       loader: 'babel'    // 一定别忘记创建.babelrc的babel配置文件,这个文件是用来设置转码的规则和插件的
-    },{
+    }, {
       test: /\.css$/,
       loader: "style!css"
     }]
-  }
+  },
+  plugins: [
+    new UglifyJSPlugin()
+  ]
 };
